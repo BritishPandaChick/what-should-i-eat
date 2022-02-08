08eee01d5d18e43e500b75bf53682c503a04e115
BritishPandaChick <sbartley1080@gmail.com>
2022-02-07 21:09:15 -0500

    Fix gemspec file. Add code to what_should_i_eat.rb and exe files

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "what_should_i_eat/version"
+require 'http'
+require 'json'
 
 module WhatShouldIEat
-  class Error < StandardError; end
-  # Your code goes here...
+  def WhatShouldIEat.fresh 
+    result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s 
+    recipes = JSON.parse(result)
+    name = recipes.first['items'].first['label']
+    url = recipes.first['items'].first['url']
+    puts name 
+    puts url
+  end
 end
